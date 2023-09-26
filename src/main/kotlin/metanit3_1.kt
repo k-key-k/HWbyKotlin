64ec4621663ee095642cc31cad07eeb020d35234
k-key-k <vaizat04@mail.ru>
2023-09-27 02:20:42 +0300

    Initial commit

---
+++ src/main/kotlin/metanit3_1.kt
@@ -0,0 +1,24 @@
+fun displayUser(name: String, age: Int = 18, position: String = "Unemployement"){
+    println("Name: $name Age: $age Position: $position")
+}
+
+fun displayUser2(age: Int = 18, name: String) {
+    println("\nName: $name Age: $age")
+}
+
+fun double(nums: IntArray){
+    nums[0] = nums[0] * 2
+    println("\nЗначение в функции double: ${nums[0]}")
+}
+fun main() {
+
+    displayUser("Danis", 18, "Manager")
+    displayUser("Jade", 40)
+    displayUser("Emil")
+
+    displayUser2(name = "Jambo", age = 13)
+
+    var nums = intArrayOf(4, 5, 6)
+    double(nums)
+    println("\nЗначение в функции main: ${nums[0]}")
+}
