1059af0f230a9352efe8787797cf2170fa1c0813
k-key-k <vaizat04@mail.ru>
2023-09-27 09:37:15 +0300

    Initial commit

---
+++ src/main/kotlin/metanit3_3.kt
@@ -0,0 +1,23 @@
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun checkAge(age: Int) {
+    if (age < 0 || age > 110){
+        println("Invalid age")
+        return
+    }
+    println("age is valid")
+}
+fun main(){
+    val a = sum(5, 3)
+    val b = sum(10, 2)
+    val c = sum(6, 9)
+    println("$a, $b, $c")
+
+    checkAge(-100)
+    checkAge(5)
+}
+
+
+
---
+++ src/main/kotlin/metanit3_4.kt
@@ -0,0 +1,27 @@
+fun square(x: Int) = x * x
+
+fun compareAge(age1: Int, age2: Int){
+    fun ageIsValid(age: Int) = age > 0 && age < 111
+
+    if (!ageIsValid(age1) || !ageIsValid(age2)) {
+        println("Invalid age")
+        return
+    }
+
+    when {
+        age1 == age2 -> println("age1 == age2")
+        age1 > age2 -> println("age1 > age2")
+        age1 < age2 -> println("age1 < age2")
+    }
+}
+
+fun main(){
+    val a = square(5)
+    val b = square(3)
+    println("$a, $b")
+
+    compareAge(30, 20)
+    compareAge(-2, 3)
+    compareAge(3, -10)
+    compareAge(20, 20)
+}
